69e85c05554822f1f4c024c2c2c791ffb795be08
manenenduoc <ma.nenen@duocuc.cl>
2025-10-25 12:39:20 -0300

    login y registro
    arreglo de css de login y creacion de carpetas de registro

--- Fase_2/Evidencias_Proyecto/Evidencias de Sistema/Aplicación/APP_POZOS/AppPozos/src/app/app-routing.module.ts
+++ Fase_2/Evidencias_Proyecto/Evidencias de Sistema/Aplicación/APP_POZOS/AppPozos/src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
+  },  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
   },
+
 ];
 
 @NgModule({
